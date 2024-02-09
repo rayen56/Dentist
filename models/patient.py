8118a8ab3695462570312c3b2022667c723f4526
Rayenghanmi <rayenghanmi56@gmail.com>
2024-02-09 15:26:02 +0100

    Patient

--- models/patient.py
+++ models/patient.py
@@ -16,6 +16,19 @@ class DentistPatient(models.Model):
         string='Gender')
     image = fields.Image(string='Image')
 
+    #Medical History Fields
     allergies = fields.Text(string='Allergies')
     medications = fields.Text(string='Medications')
     health_conditions = fields.Text(string='Health Conditions')
+
+    # Dental Records Fields
+    oral_health_information = fields.Text(string='Oral Health Information')
+    past_treatments = fields.Text(string='Past Treatments')
+    x_rays = fields.Binary(string='X-Rays')
+    photos = fields.Binary(string='Photos')
+    treatment_plans = fields.Text(string='Treatment Plans')
+    notes = fields.Text(string='Notes')
+    progress_updates = fields.Text(string='Progress Updates')
+
+    # Image Field for Dental Images
+    dental_images = fields.Binary(string='Dental Images', attachment=True)
