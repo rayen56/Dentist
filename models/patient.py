a4e8f62a49967acc360ac133b6576ae5b6aa4516
Rayenghanmi <rayenghanmi56@gmail.com>
2024-02-09 16:32:15 +0100

    33

--- models/patient.py
+++ models/patient.py
@@ -8,27 +8,28 @@ class DentistPatient(models.Model):
     address = fields.Text(string="Address")
     phone = fields.Char(string='Phone')
     email = fields.Char(string='Email')
-    date_of_birth = fields.Date(string='Date of Birth')
+    date_of_birth = fields.Date(string='Date of Birth', help='Enter the patient\'s date of birth')
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female'),
         ('other', 'Other')],
         string='Gender')
-    image = fields.Image(string='Image')
 
-    #Medical History Fields
+    # Personal Image
+    image_1920 = fields.Image(string='Image')
+
+    # Medical History Fields
     allergies = fields.Text(string='Allergies')
     medications = fields.Text(string='Medications')
-    health_conditions = fields.Text(string='Health Conditions')
 
     # Dental Records Fields
     oral_health_information = fields.Text(string='Oral Health Information')
     past_treatments = fields.Text(string='Past Treatments')
-    x_rays = fields.Binary(string='X-Rays')
-    photos = fields.Binary(string='Photos')
+
+    # Image Fields for Dental Records
+    x_rays = fields.Image(string='X-Rays', help='Upload X-Ray images')
+    dental_images = fields.Image(string='Dental Images', attachment=True, help='Upload dental images')
+
     treatment_plans = fields.Text(string='Treatment Plans')
     notes = fields.Text(string='Notes')
     progress_updates = fields.Text(string='Progress Updates')
-
-    # Image Field for Dental Images
-    dental_images = fields.Binary(string='Dental Images', attachment=True)
